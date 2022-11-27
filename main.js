c2ce8df640f17f8dddc7141bdb8f0f6de149bfd6
GianAlingog <76099610+GianAlingog@users.noreply.github.com>
2022-11-27 13:52:08 +0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let activeIndex = 0;
+
+const groups = document.getElementsByTagName("article");
+
+const handleRightClick = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+  const nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "inactive";
+  nextGroup.dataset.status = "active";
+
+  activeIndex = nextIndex;
+};
+
+const handleLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+  const nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "inactive";
+  nextGroup.dataset.status = "active";
+  
+  activeIndex = nextIndex;
+}
